1832f1b84d7bccbe26e6636f9b6234e780215c2d
Drew X <drewxdvst@outlook.com>
2018-02-10 22:12:39 +0200

    fix  argument parsing

--- explore_biome.R
+++ explore_biome.R
@@ -1,5 +1,22 @@
+#! /usr/bin/env Rscript
+
+require(argparse)
+cwd = getwd()
+parser <- ArgumentParser()
+parser <- ArgumentParser(description="Process sanger ab1 reads and generate fasta files")
+parser$add_argument("biom", help="input folder where abi files are located.")
+parser$add_argument("-o","--output-folder", dest="outDir", default=cwd,
+                    help="directory to write output files")
+
 library(phyloseq)
 library(ggplot2)
 library(gridExtra)
 library(dunn.test)
-source("/home/gerrit/workspace/amw/src/microbiome_custom_functions.R")
+source("/home/drewx/Documents/Paribus/microbiome_custom_functions.R")
+
+biom <- args$biom
+inDir  <- args$inDir
+
+phy <- import_biom(BIOMfilename = biom, verbose = TRUE)
+ntaxa(phy)#(number of OTUs)
+
