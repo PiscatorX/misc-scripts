fe48995731b55f4ad26b9f9fcc745b49e1a5010d
Drew X <drewxdvst@outlook.com>
2018-02-11 22:49:58 +0200

    add tsv file write output

--- summary2krona.py
+++ summary2krona.py
@@ -13,10 +13,10 @@ class Summary2Krona(object):
         parser.add_argument('qiime_summary')
         parser.add_argument('-p','--prefix', default="D_L__",help = "prefix 'L' for digits (default 0-9)")
         args = parser.parse_args()
-        self.summary = args.qiime_summary
+        self.qiime_summary = args.qiime_summary
         self.prefix = args.prefix.replace('L','{}')
-        self.summary_data = open(self.summary).read()
-        self.krona_file = '_'.join(['krona', self.summary_data])
+        self.summary_data = open(self.qiime_summary).read()
+        self.krona_file = '_'.join(['krona', self.qiime_summary])
 
         
     def parse2krona(self):
@@ -28,9 +28,9 @@ class Summary2Krona(object):
                              if not row[0].startswith('#') ]
 
         with open(self.krona_file, 'w') as fp:
+            csv_writer = csv.writer(fp, dialect='excel-tab') 
             for row in csv.reader(self.summary_data, dialect='excel-tab'):
-                line = [row[-1]]+row[:-2]
-                fp.writeline(line)
+                csv_writer.writerow([row[-1]]+row[:-2])
             
         
 if __name__ == '__main__':
