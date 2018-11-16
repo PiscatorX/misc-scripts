00edec9cfd6286b0b36e0fc840834984d1f4e3aa
Andrew Ndhlovu <A0047731@wits.ac.za>
2018-11-16 12:04:07 +0200

    script for getting reps in text file within brackets

---
+++ curlrefs.py
@@ -0,0 +1,19 @@
+
+
+
+class Curlrefs(object):
+
+    def __init__(self, fname):
+
+        self.fname = fname
+
+    def get_data(self):
+
+        print self.fname
+
+
+
+
+curlref = Curlrefs(object)
+curlref.get_data()
+
