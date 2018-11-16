04e81b977e5b88aa24157d81a45facc81911fc52
Andrew Ndhlovu <username@myEmail.com>
2018-11-16 17:53:32 +0200

    fount a greed but sufficient regex

--- curlrefs.py
+++ curlrefs.py
@@ -1,19 +1,28 @@
-
+import re
 
 
 class Curlrefs(object):
 
     def __init__(self, fname):
 
-        self.fname = fname
-
+        self.fname  = fname
+        
     def get_data(self):
+        regex1 = re.compile("(\(\w+,* \d+\)|\(\w+ and \w+, \d+\)|\(\w+ .*;.*\d+\))")
+        #(Lockshin and Williams, 1964)
+        regex2 = re.compile("\(\w+ and \w+, \d+\)")
+        data = open(self.fname).read()
 
-        print self.fname
+        print re.findall(regex1, data)
+        
+            
+                    
+        
+            
+    
 
 
-
-
-curlref = Curlrefs(object)
+curlref = Curlrefs("CH1_LitRevPhDX.txt.bak")
+#curlref = Curlrefs("test.txt")
 curlref.get_data()
 
