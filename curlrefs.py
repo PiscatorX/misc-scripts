4525ec0b9aeda49f7b26808236a011f33c0787eb
Andrew Ndhlovu <drewxdvst@outlook.com>
2018-11-17 15:43:07 +0200

    fixed argarparse and regexp

--- curlrefs.py
+++ curlrefs.py
@@ -1,28 +1,29 @@
+from collections import *
+import argparse
 import re
 
 
 class Curlrefs(object):
 
-    def __init__(self, fname):
+    def __init__(self):
 
-        self.fname  = fname
+        parser = argparse.ArgumentParser("Extract refs from text file e.g  (Drewx et al., 2018)")
+        parser.add_argument('textfile', help="file where refs must be extracted from")
+        args = parser.parse_args()
+        self.data = open(args.textfile).read()
+        self.regexps = [re.compile("(\(\w+ \&{0,1}(et|and|) \w+[.,],{0,1} \d{4}\w{0,1}(;.*\)|\)))")]
+        #Allows for additional regular expressions
+        
         
     def get_data(self):
-        regex1 = re.compile("(\(\w+,* \d+\)|\(\w+ and \w+, \d+\)|\(\w+ .*;.*\d+\))")
-        #(Lockshin and Williams, 1964)
-        regex2 = re.compile("\(\w+ and \w+, \d+\)")
-        data = open(self.fname).read()
 
-        print re.findall(regex1, data)
-        
-            
-                    
-        
-            
+        for regex  in self.regexps:
+            #allow
+            references = re.finditer(regex, self.data)
+            for ref in Counter(([ ref.group() for ref in references ])):
+                print(ref)
+                            
     
-
-
-curlref = Curlrefs("CH1_LitRevPhDX.txt.bak")
-#curlref = Curlrefs("test.txt")
+curlref = Curlrefs()
 curlref.get_data()
 
