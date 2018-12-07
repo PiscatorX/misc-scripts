ba144ddb3cefdc245625f1e72898b8e1dde8a759
Andrew Ndhlovu <drewxdvst@outlook.com>
2018-12-07 12:45:15 +0200

    count workds

---
+++ wc_gui.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import tkinter
+
+class CountWords(object):
+
+    def __init__(self, master):
+
+        self.master = master
+        master.title("Count words")
+        
+
+top = tkinter.Tk()
+
+top.mainloop()
