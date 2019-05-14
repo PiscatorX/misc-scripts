6b99970a510b0e06de1373f00a2a975f65e2a00f
Andrew Ndhlovu <drewxdvst@outlook.com>
2019-05-15 01:28:16 +0200

    added iso time column

--- get_ctd.py
+++ get_ctd.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
-import  csv
-import  glob
-import  sys
-import  pprint
-import  argparse
-import  pandas as pd
+import os
+import csv
+import glob
+import sys
+import pprint
+import argparse
+import pandas as pd
 import collections
 import datetime 
 
@@ -30,6 +31,7 @@ long : 18.10633
         parser.add_argument('-g', '--glob_pattern', default="*St6.csv", help = "file pattern with wild cards matching files to be processed")
         parser.add_argument('-o', '--csv_save', default="ctd_all.csv", help = "output file to save data")
         parser.add_argument('-t', '--time', help = "output file to save date and times ")
+        parser.add_argument('-f', '--force', help = "force file overwrite", action='store_true')
         args = parser.parse_args()
         self.metavars = collections.OrderedDict([ line.strip().split('=')
                                for line in open(args.metavars).read().splitlines()
@@ -38,66 +40,78 @@ long : 18.10633
         assert any(self.csv_files), "No files matching glob {} ".format(args.glob_pattern)
         self.strip_comma = lambda x: x.replace(',','')
         self.ctd_dataframes = []
-        self.file_header = False
+        
         self.file_units = False
         self.csv_save = args.csv_save
+        if os.path.exists(self.csv_save):
+            if not args.force:
+                sys.stderr.write("\noutput file `{}` exists, use -f/--force to overwrite.\n\n".format(self.csv_save))
+                sys.exit(1)
         self.time = args.time
         if self.time:
             self.time_fobj = open(self.time, "w")
-            print >> self.time_fobj,"Date,time"
+            print("Date,time",file=self.time_fobj)
         #YEAR,MONTH,DAY,HOUR,MINUTE,SEC
 
         
     def get_data(self):
         
         for csv_fname  in self.csv_files:
-            print csv_fname
+            #print(csv_fname)
             self.parse_csv(csv_fname)    
         self.ctd_all = pd.concat(self.ctd_dataframes, ignore_index=True)
         
             
         
     def parse_csv(self, csv_fname):
-        
+
+    
         with open(csv_fname) as fp:
             ctd_file = csv.reader(fp)
-            col_header = False
+            self.col_header = False
             for i,row in enumerate(ctd_file):
                 if any(row):
                     if i == 0:
                         date, time = filter(any, row)
-                        d, t  = self.get_time(date, time)
-                        map(self.metavars.update, [d, t])
+                        d, t, time_ISO8601  = self.get_time(date, time)
+                        self.metavars.update({**d, **t, **time_ISO8601})
+                        
                         
                     elif i == 1:
-                        col_header = self.dedup(map(self.strip_comma, row))
-                        data = dict((param,[]) for param in  col_header)
-                        if not self.file_header:
-                            self.file_header = self.metavars.keys() + col_header
-                            
-                            
+                        self.col_header = self.dedup(map(self.strip_comma, row))
+                        data = dict((param,[]) for param in  self.col_header)
+                         
                     elif i == 2:
                         if not self.file_units:
                             self.file_units = row
-                    elif col_header:
-                       for param, reading in zip(col_header, row):
+                    elif self.col_header:
+                       for param, reading in zip(self.col_header, row):
                            data[param].append(reading)
-            df = pd.DataFrame(data)
+                       df = pd.DataFrame(data)
+                       
             for i,(col, value)  in enumerate(self.metavars.items()):
                 df.insert(i, col, value)
+                #print(df)
+            
             self.ctd_dataframes.append(df)
 
        
     def get_time(self, date, time):
 
+        
         dt_obj = datetime.datetime.strptime(date,'%d-%b-%y')
         time_str = str(datetime.timedelta(seconds=float(time)))
         time_obj = datetime.datetime.strptime(time_str, '%H:%M:%S')
+
+        date_time_str = ' '.join([date, time_str])
+        date_time_obj = datetime.datetime.strptime(date_time_str,'%d-%b-%y %H:%M:%S')
+        time_ISO8601 = {'time_ISO8601': date_time_obj.isoformat() }
         
         d = collections.OrderedDict(map(lambda v: (v, getattr(dt_obj, v)), ['year','month','day']))
         t =  collections.OrderedDict(map(lambda v: (v, getattr(time_obj, v)), ['hour','minute','second']))
-        print >> self.time_fobj,"{},{}".format(dt_obj.strftime("%d/%m/%y"),time_str)
-        return d, t
+        if self.time:
+            print("{},{}".format(dt_obj.strftime("%d/%m/%y"),time_str,file=self.time_fobj))
+        return d, t, time_ISO8601 
         
     def dedup(self, dup_list):
         
@@ -118,19 +132,23 @@ long : 18.10633
         return deduped_list
 
     
+    
     def save_ctd(self):
+
         
         with open(self.csv_save, 'w') as fp:
             csv_writer = csv.writer(fp)
-            csv_writer.writerow(self.file_header)
-            #csv_writer.writerow([ '' for _ in self.metavars.keys() ] +  self.file_units)    
+            csv_writer.writerow(list(self.metavars.keys()) + self.col_header )
+            csv_writer.writerow([ '' for _ in self.metavars.keys() ] +  self.file_units)    
         self.ctd_all.to_csv(self.csv_save, header=False, mode='a', index=False)
         if self.time:
-            self.time_fobj.close() 
-        print "\nCTD data sucessefully saved to ", self.csv_save
+            self.time_fobj.close()
+        print(self.ctd_all)
+        print("\nCTD data sucessefully saved to ", self.csv_save)
         
 
-        
-ctd = GetCTD()
-ctd.get_data()
-ctd.save_ctd()
+if __name__ == '__main__':       
+    ctd = GetCTD()
+    ctd.get_data()
+    ctd.save_ctd()
+    
