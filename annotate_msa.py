9212d70a0aa3421c42eabcc62b8d1910b9feb15c
Andrew Ndhlovu <drewxdvst@outlook.com>
2019-07-23 17:15:18 +0200

    misc scripts for handling alignments

---
+++ annotate_msa.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env  python
+
+from Bio import AlignIO
+import csv
+import argparse
+import pprint
+import sys
+
+
+def annotate_ref(msa_fobj, map_fobj,  msa_informat, outfile_fobj, msa_outformat):
+
+    map_data = dict([ line.split("\t") for line in map_fobj.read().splitlines() if line ])
+    msa_data = AlignIO.read(msa_fobj, msa_informat)
+    annotated_align = []
+    for align in msa_data:
+        seq_id = align.id
+        try:
+            genus, species = map_data[align.id].split()
+            #annotation = ''.join([genus[0]+'.',species,"[{}]".format(seq_id)])
+            #annotation = "{}[{}]".format(map_data[align.id],seq_id)
+            annotation = "{} {}".format(genus, species)
+            align.id = annotation
+        except KeyError:
+            pass
+        annotated_align.append(align)
+    
+    AlignIO.write(AlignIO.MultipleSeqAlignment(annotated_align),
+                  outfile_fobj,
+                  msa_outformat)
+    
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser("Generate ref file for Unipro fasta file")
+    parser.add_argument('msa_file',help ="multiple sequence alignment (MSA) file", type=argparse.FileType('r'))
+    parser.add_argument('-m','--map', type=argparse.FileType('r'), required = True)
+    parser.add_argument('-i','--informat', default="clustal", help ="MSA format")
+    parser.add_argument('-o','--outfile', type=argparse.FileType('w'), help ="MSA output file",  required=True)
+    parser.add_argument('-v','--outformat', default="clustal", help ="MSA output format")
+    args = parser.parse_args()
+    annotate_ref(args.msa_file, args.map, args.informat, args.outfile, args.outformat)
+
+
+
+
+
+        
+    
+
+        
---
+++ annotate_seq.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env  python
+
+from Bio import SeqIO
+import csv
+import argparse
+import pprint
+import sys
+
+
+def annotate_ref(seq_fobj, map_fobj,  informat, outfile_fobj, outformat):
+
+    map_data = dict([ line.split("\t") for line in map_fobj.read().splitlines() if line ])
+    seq_fobj = SeqIO.parse(seq_fobj, informat)
+    annotated_seq = []
+    for seq in seq_fobj:
+        seq_id = seq.id
+        seq.description = ''
+        try:
+            genus, species = map_data[seq.id].split()
+            #annotation = ''.join([genus[0]+'.',species,"[{}]".format(seq_id)])
+            #annotation = "{}[{}]".format(map_data[align.id],seq_id)
+            annotation = "{}_{}".format(genus, species)
+            seq.id = annotation
+        except KeyError:
+            pass
+        annotated_seq.append(seq)
+    
+    SeqIO.write(annotated_seq,
+                  outfile_fobj,
+                    outformat)
+    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser("Generate ref file for Unipro fasta file")
+    parser.add_argument('seq_file',help ="sequence file", type=argparse.FileType('r'))
+    parser.add_argument('-m','--map', type=argparse.FileType('r'), required = True)
+    parser.add_argument('-i','--informat', default="fasta")
+    parser.add_argument('-o','--outfile', type=argparse.FileType('w'),  required=True)
+    parser.add_argument('-v','--outformat', default="fasta")
+    args = parser.parse_args()
+    annotate_ref(args.seq_file, args.map, args.informat, args.outfile, args.outformat)
+
+
+
+
+
+        
+    
+
+        
