819ff2dc17cad464b21d04948312af6f4eda40b4
Andrew Ndhlovu <drewxdvst@outlook.com>
2019-10-13 15:28:06 +0200

    add misc script for dealing with dots . mixed with dashes- in alignment

---
+++ msa_rmdot.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+from Bio.Align import MultipleSeqAlignment
+from Bio.SeqRecord import SeqRecord
+from Bio.Alphabet import generic_dna
+from Bio import AlignIO
+from Bio.Seq import Seq
+import argparse
+import sys
+
+import os
+
+__author__ = "Andrew Ndhlovu"
+__copyright__ = "Copyright 2019"
+__license__ = "GPL"
+__version__ = "3"
+__maintainer__ = "Andrew Ndhlovu"
+__email__ = "drewxdvst@outlook.com"
+
+
+def rm_dot(args):
+
+    if not args.outfile:
+         outfile = ''.join([args.infile.name.replace(args.infmt, ''), args.ofmt])
+
+    msa  = AlignIO.read(args.infile, args.infmt)
+    dd_msa =  []
+    for i,alignment in enumerate(msa,1):
+        seq = SeqRecord(Seq(str(alignment.seq).replace(".","-"), generic_dna), id=alignment.id) 
+        dd_msa.append(alignment)
+        
+    AlignIO.write(MultipleSeqAlignment(dd_msa), open(outfile,'w'), args.ofmt)
+
+    
+if  __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="simple Biopython format convertor, converts gb to fasta by default")
+    parser.add_argument('infile', action='store',  type=argparse.FileType('r'))
+    parser.add_argument('-i','--in-format', dest='infmt', action='store', default='clustal', type=str)
+    parser.add_argument('-f','--out-format', dest='ofmt', action='store', default='clustal', required=False, type=str)    
+    parser.add_argument('-o','--outfile', dest='outfile', action='store', type=str)
+    args = parser.parse_args()
+    rm_dot(args)
