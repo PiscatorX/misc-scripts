7e0f6e23bbb956ee32cb4bdfefd6c606454102e6
U-HP-LAPTOP\DrewX <drewxdvst@outlook.com>
2021-01-23 15:15:50 +0200

    add command line args

--- group_medium.py
+++ group_medium.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python
 import pandas as pd
 import argparse
+import sys
 import os
 
 
-def group_medium(fname, group, *variables):
+def group_medium(fname, group, check_headers, sep, *variables):
 
-    data_df = pd.read_csv(fname)
+    data_df = pd.read_csv(fname, sep = sep)
+    
+    if check_headers:
+        print("Columns:\t{}".format(data_df.columns))
+        sys.exit(0)
+        
     for var in variables:
         new_var = '_'.join(['median', var])
         data_df.loc[:,new_var] = pd.NA
         data_df[new_var] = data_df.groupby(group)[var].transform('median')
-
+        
     outfname = os.path.basename(fname.name)
     #print(outfname)
     data_df.to_csv('modified_' + outfname, sep = "\t")
@@ -19,8 +25,10 @@ def group_medium(fname, group, *variables):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("Annotate an msa file using a map file")
     parser.add_argument('data_fname', type=argparse.FileType('r'))
-    parser.add_argument('-g','--group', help = "grouping column",  required=True)
-    parser.add_argument('-v','--variables', nargs='+', help = "variable columns", required=False, default = ["AREA"])
+    parser.add_argument('-g','--group', help = "grouping column", default = "shapeid", required=False)
+    parser.add_argument('-s','--sep',  default=',')
+    parser.add_argument('-c','--check_headers', action='store_true')
+    parser.add_argument('-v','--variables', nargs='+', help = "variable columns", required=False, default = ['x','y'])
     args = parser.parse_args()
-    group_medium(args.data_fname, args.group, *args.variables)
+    group_medium(args.data_fname, args.group, args.check_headers,  args.sep, *args.variables)
 
